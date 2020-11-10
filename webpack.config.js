b24f8f9a1e7c7eea25a5fcfda886020aeef04962
TuyizeeAnastase <42033331+TuyizeeAnastase@users.noreply.github.com>
2020-11-10 12:14:41 +0200

    chore(setup):adding dummy route[finishes:#RmsEGBhd]
    chore(redux): Setting up Redux
    
    Installation of the library
    Creation of counter
    Creation of store
    
    [(finishes) #VGGGie9k]

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,10 @@ module.exports = {
         test: /\.scss$/,
         use: ["style-loader", "css-loader", "sass-loader"],
       },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"]
+      },
     ],
   },
   plugins: [new HtmlWebpackPlugin({ template: "./public/index.html" })],
