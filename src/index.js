3ca43901109ce3027dc109a338d0dcc428fe7a65
Mugema123 <mugemaleo@gmail.com>
2020-11-13 07:14:45 +0200

    ch(setupTest):setup-testing-environment-with-jest-and-enzyme

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App'
+import ReactDom from 'react-dom';
+import App from './components/App';
 
-ReactDOM.render(<App />,document.getElementById('root'));
+const title = 'React with Webpack and Babel';
+ReactDom.render(<App title={title} />, document.getElementById("root"));
+module.hot.accept();
