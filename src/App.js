e909173e5cd7f0798aadfe1cc3215085c6e86240
Ally <el.ally741@gmail.com>
2020-11-24 13:59:26 +0200

    chore(sass): setting up sass
    installation of the package
    Creation of sass folder
    Creation of sass file
    Creation of sass folder
    creation of css file
    
    [(finishes)]

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import * as actions from "./redux/actions/counter";
+import './scss/styles.scss'
 
 class App extends Component {
   render() {
--- webpack.config.js
+++ webpack.config.js
@@ -1,37 +1,37 @@
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const ip = require("ip");
-const hostname = ip.address() || 'localhost';
+const hostname = ip.address() || "localhost";
 
 module.exports = {
-  entry: './src/index.js',
-  output: {path: path.join(__dirname, "/dist"),   
-  filename: "bundle.js"
- },
- devServer: {
-  contentBase: path.resolve(__dirname, './build'),
-  publicPath: '/',
-  host: `${hostname}`,
-  port: 3020,
-  hot: true,
-  inline: true,
-  contentBase: './',
-  watchOptions: {
-      poll: true
+  entry: "./src/index.js",
+  output: { path: path.join(__dirname, "/dist"), filename: "bundle.js" },
+  devServer: {
+    contentBase: path.resolve(__dirname, "./build"),
+    publicPath: "/",
+    host: `${hostname}`,
+    port: 3020,
+    hot: true,
+    inline: true,
+    contentBase: "./",
+    watchOptions: {
+      poll: true,
+    },
+    compress: true,
+    open: true,
   },
-  compress: true,
-  open: true,
-},
   module: {
     rules: [
       {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
-        use: 'babel-loader',
+        use: "babel-loader",
+      },
+      {
+        test: /\.scss$/,
+        use: ["style-loader", "css-loader", "sass-loader"],
       },
     ],
   },
-  plugins: [
-    new HtmlWebpackPlugin({ template: './public/index.html' }),
-  ],
+  plugins: [new HtmlWebpackPlugin({ template: "./public/index.html" })],
 };
